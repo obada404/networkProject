55f5bba899cc57e8a14c17ee8577bae7e2f66ec8
Eng_Obada Qasrawi <Eng_Obada Qasrawi@192.168.1.110>
2021-04-19 01:22:01 +0300

    first

---
+++ src/p2p_udp_chat_part1/P2p_UDP_chat_part1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2p_udp_chat_part1;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Eng_Obada Qasrawi
+ */
+public class P2p_UDP_chat_part1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        
+      chat a =   new  chat();
+      
+      a.setVisible(true);
+      a.setSize(800, 500);
+      
+    }
+    
+}
